1ba8566651d3400bfeca7648d5e82abd8ce584cb
Brubis <140927735+Brubis-05@users.noreply.github.com>
2024-04-17 10:10:49 -0300

    atualização
    atualização dos arquivos

--- controls.js
+++ controls.js
@@ -1,6 +1,13 @@
-export default {
-    jump: new KeyboardEvent('keydown', {key: 'Space', keyCode: 32}),
+/*export default {
+    jump: new KeyboardEvent('keydown', {key: 'Space', keyCode: 32});
     dispatch(event) {
+        document.dispatchEvent(this[event]);
+    }
+}*/
+
+export default {
+    jump: new KeyboardEvent('keydown', {key: 'Space', keyCode:32}),
+    dispatch (event) {
         document.dispatchEvent(this[event]);
     }
 }
